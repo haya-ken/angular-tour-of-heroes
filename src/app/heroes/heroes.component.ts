9ace2315df7c3b6e1c822d921f42f7441ff3dc98
ken__ken <kenzi0716@gmail.com>
2024-02-09 16:32:16 +0900

    4. Create a feature component (フィーチャーコンポーネントの作成、hero-detail追加し、heroesから一部detailに移す)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  NgFor,
-  NgIf,
-  UpperCasePipe,
-} from '@angular/common';
-import { FormsModule }  from '@angular/forms';
+// import { NgFor, NgIf, UpperCasePipe, } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+// import { FormsModule }  from '@angular/forms';
 import { Hero }         from '../hero';
 import { HEROES }       from '../mock-heroes';
 
@@ -13,15 +11,12 @@ import { HEROES }       from '../mock-heroes';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [
-    FormsModule,
-    NgIf,
-    NgFor,
-    UpperCasePipe,
-  ],
+  // imports: [FormsModule, NgIf, NgFor, UpperCasePipe,],
+  imports: [HeroDetailComponent, NgFor],
 })
 export class HeroesComponent {
   heroes = HEROES;
+
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
